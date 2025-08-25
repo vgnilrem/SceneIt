59fbd00791054245fb449b5069851a85c48b11d4
Jordan <jordinholenhart@gmail.com>
2025-08-24 19:24:42 -0700

    TrendingShows.jsx created and Home.jsx edited to render the component.
    Trending shows component added to home page using API.
    Home.jsx imports the component and renders with the hero. trending playlists placeholder to be added.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import TrendingShows from "../components/TrendingShows.jsx";
 
 export default function Home() {
     return (
       <div>
         <Hero />
+        <TrendingShows/>
       </div>
     );
   }
